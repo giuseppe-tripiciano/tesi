093285f2cffbbb3ddf30119854ff72c02834405e
Bringernaves <85127296+Bringernaves@users.noreply.github.com>
2025-12-09 13:09:05 +0100

    Add functions
    - parse_qtree: funzione che prende in input una stringa di liste
      innestate corrispondente ad un quantum tree e ne fa il parsing
      marcando i nodi.
    
    - draw_tree: funzione che utilizza la libreria graphviz per la
      costruzione di un quantum tree. Prende in input la stringa restituita
      dalla funzione precedente e restituisce l'oggetto dot in formato svg
      per la visualizzazione dell'albero.

---
+++ qtree.py
@@ -0,0 +1,64 @@
+import graphviz
+import uuid
+import re
+import ast
+from IPython.display import HTML
+
+def parse_qtree(input_str):
+    pattern = r"([a-zA-Z_]\w*\((?:[^()]|\([^()]*\))*\))"
+    quoted_str = re.sub(pattern, r"'\1'", input_str)
+    return ast.literal_eval(quoted_str)
+
+
+def draw_qtree(tree, title='Quantum Tree'):
+    dot = graphviz.Digraph()
+    dot.attr(rankdir='TB')
+    dot.attr('node', shape='box', style='rounded, filled', fillcolor='lightgrey')
+
+    dot.attr(label=f"{title}\n\n")  
+    dot.attr(labelloc='t')       
+    dot.attr(fontsize='18')      
+    dot.attr(fontname='Helvetica') 
+
+    def draw_qsubtree(subtree, parent_id=None):
+        # empty list / None
+        if not subtree: 
+            return
+
+        if isinstance(subtree, list):
+            # current node
+            current_val = subtree[0]
+            
+            # children nodes
+            children = subtree[1:] 
+        else:
+            
+            # leaf node
+            current_val = subtree
+            children = []
+
+        
+        node_id = str(uuid.uuid4())
+        label = str(current_val)
+
+        if re.match(r"^[q]\(\d+\)$", label):
+            color = 'white'      
+        else:
+            color = 'lightblue'  
+
+        dot.node(node_id, label=label, fillcolor=color)
+
+        # link to parent node
+        if parent_id:
+            dot.edge(parent_id, node_id)
+
+        # recursion on children nodes
+        for child in children:
+            draw_qsubtree(child, parent_id=node_id)
+
+    # start recursion
+    draw_qsubtree(tree)
+
+    svg_dot = dot.pipe(format='svg').decode('utf-8')
+
+    return HTML(f'<div style="width: 100%; height: 100%; display: flex; justify-content: center;">{svg_dot}</div>')
